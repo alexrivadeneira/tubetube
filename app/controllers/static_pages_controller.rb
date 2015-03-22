aed82c1eb4e6dacafe8ec94512c7dbc9a8d8cd92
Alex Rivadeneira <alexrivs@gmail.com>
2015-03-21 21:32:08 -0400

    httpparty working on test page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,4 +8,14 @@ class StaticPagesController < ApplicationController
 		@youtube_api_key = ENV["youtube_api_key"]
 	end
 
+	def test
+		wmata_api_key = ENV["wmata_api_key"]
+
+		request_url = "https://api.wmata.com/Rail.svc/json/jSrcStationToDstStationInfo?FromStationCode=A04&ToStationCode=A06&api_key="
+		request_url += wmata_api_key
+
+		@wmata_data = HTTParty.get(request_url)
+	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get "search" => "static_pages#show"
+  get "test" => "static_pages#test"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
