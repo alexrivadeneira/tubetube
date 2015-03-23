8fd8a229ff6c20047bce40610730602a67fe4cbf
Alex Rivadeneira <alexrivs@gmail.com>
2015-03-22 20:45:03 -0400

    prevent duplicate stations

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,33 +7,38 @@ class StaticPagesController < ApplicationController
 		stopcodeA = params["metro_station_a"]
 		stopcodeB = params["metro_station_b"]
 
-		wmata_request_url = "https://api.wmata.com/Rail.svc/json/jSrcStationToDstStationInfo?FromStationCode="
-		wmata_request_url += stopcodeA
-		wmata_request_url += "&ToStationCode="
-		wmata_request_url += stopcodeB
-		wmata_request_url += "&api_key="
-		wmata_request_url += ENV["wmata_api_key"]
+		if stopcodeA == stopcodeB
+			@no_time = true
+			return @no_time
 
-		@wmata_data = HTTParty.get(wmata_request_url)
+		else
+			wmata_request_url = "https://api.wmata.com/Rail.svc/json/jSrcStationToDstStationInfo?FromStationCode="
+			wmata_request_url += stopcodeA
+			wmata_request_url += "&ToStationCode="
+			wmata_request_url += stopcodeB
+			wmata_request_url += "&api_key="
+			wmata_request_url += ENV["wmata_api_key"]
 
-		youtube_request_url = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=10&videoDuration="
-		
-		if @wmata_data["StationToStationInfos"][0]["RailTime"].to_i < 5
-			youtube_request_url += "short"
-		elsif @wmata_data["StationToStationInfos"][0]["RailTime"].to_i >= 5 && (@wmata_data["StationToStationInfos"][0]["RailTime"].to_i < 20)
-			youtube_request_url += "medium"
-		elsif @wmata_data["StationToStationInfos"][0]["RailTime"].to_i >= 20
-			youtube_request_url += "long"
+			@wmata_data = HTTParty.get(wmata_request_url)
+
+			youtube_request_url = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=10&videoDuration="
+			
+			if @wmata_data["StationToStationInfos"][0]["RailTime"].to_i < 5
+				youtube_request_url += "short"
+			elsif @wmata_data["StationToStationInfos"][0]["RailTime"].to_i >= 5 && (@wmata_data["StationToStationInfos"][0]["RailTime"].to_i < 20)
+				youtube_request_url += "medium"
+			elsif @wmata_data["StationToStationInfos"][0]["RailTime"].to_i >= 20
+				youtube_request_url += "long"
+			end
+
+			youtube_request_url += "&key="
+
+			youtube_api_key = ENV["youtube_api_key"]
+			youtube_request_url += youtube_api_key
+			puts(youtube_request_url)
+
+			@youtube_data = HTTParty.get(youtube_request_url)
 		end
-
-		youtube_request_url += "&key="
-
-		youtube_api_key = ENV["youtube_api_key"]
-		youtube_request_url += youtube_api_key
-		puts(youtube_request_url)
-
-		@youtube_data = HTTParty.get(youtube_request_url)
-
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   post "submit" => "static_pages#submit"
   get "home" => "static_pages#home"
 
+  root "static_pages#home"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
